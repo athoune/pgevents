56f5049493ce9265a749032c5f22d92b80ec6744
Mathieu Lecarme <mlecarme@bearstech.com>
2012-10-10 22:45:49 +0200

    Python client POC.

---
+++ python/src/pgevents.py
@@ -0,0 +1,33 @@
+import select
+import psycopg2
+import psycopg2.extensions
+
+
+def dispatcher(conn, cb):
+    conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+    curs = conn.cursor()
+    curs.execute("LISTEN test;")
+
+    print "Waiting for notifications on channel 'test'"
+    lastseen = 'epoch'
+    while 1:
+        if select.select([conn], [], [], 5) == ([], [], []):
+            print "Timeout"
+        else:
+            conn.poll()
+            while conn.notifies:
+                notify = conn.notifies.pop()
+                print "Got NOTIFY:", notify.pid, notify.channel, notify.payload
+                curs.callproc('on_event', ['test', lastseen])
+                for record in curs:
+                    lastseen = record[0]
+                    cb(0, record)
+
+
+def _notify(ts, payload):
+    print payload
+
+DSN = "dbname=pubsub"
+conn = psycopg2.connect(DSN)
+dispatcher(conn, _notify)
