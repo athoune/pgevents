92e38659ab3449929edce197d89da76792c361f0
Mathieu Lecarme <mlecarme@bearstech.com>
2012-10-21 00:02:23 +0200

    Events should be read synchronously.

--- python/src/pistil_pgevents.py
+++ python/src/pistil_pgevents.py
@@ -1,28 +1,15 @@
 import select
-import socket
-import time
-import os
 
 from pistil.arbiter import Arbiter
 from pistil.worker import Worker
-from pistil.tcp.sync_worker import TcpSyncWorker
-from pistil.tcp.arbiter import TcpArbiter
-from pistil.util import parse_address, close
 
 import psycopg2
 import psycopg2.extensions
 
-from event import Event, serialize, unserialize, SocketRW
+from event import Event
 
 
-class EventArbiter(TcpArbiter):
-    def on_init(self, conf):
-        TcpArbiter.on_init(self, conf)
-        # we return a spec
-        return (conf['event_worker'], 30, "worker", {}, "worker",)
-
-
-class PgEventListener(Worker):
+class PgEventWorker(Worker):
     def on_init(self, conf):
         self.conn = psycopg2.connect(conf['dsn'])
         self.conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
@@ -30,7 +17,6 @@ class PgEventListener(Worker):
         self.curs = self.conn.cursor()
         self.curs.execute("LISTEN test;")
         self.lastseen = 'epoch'
-        self.address = parse_address(conf['address'])
         self.channel = conf['channel']
         self.fetch_events()  # Fetch old events.
 
@@ -41,10 +27,7 @@ class PgEventListener(Worker):
             evt = Event()
             evt.channel = 'test'
             evt.payload = record[1]
-            sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-            sock.connect(self.address)
-            serialize(evt, SocketRW(sock))
-            close(sock)
+            self.on_event(evt)
 
     def handle(self):
         if select.select([self.conn], [], [], 5) == ([], [], []):
@@ -58,38 +41,22 @@ class PgEventListener(Worker):
             while self.conn.notifies:
                 self.conn.notifies.pop()
 
-
-class EventWorker(TcpSyncWorker):
-    def handle(self, sock, addr):
-        #FIXME implement timeout
-        evt = unserialize(SocketRW(sock))
-        self.do_event(evt)
-        close(sock)
-
-    def do_event(self, event):
+    def on_event(self, evt):
         raise NotImplementedError()
 
+
 if __name__ == '__main__':
-    af = "/tmp/pgevents.sock"
-    try:
-        os.remove(af)
-    except Exception:
-        pass
 
-    class TestWorker(EventWorker):
-        def do_event(self, evt):
+    class TestWorker(PgEventWorker):
+        def on_event(self, evt):
             print "###### Event", self.pid, evt.channel, evt.payload
-            time.sleep(1)
 
-    conf = {"num_workers": 5,
-            "address": "unix:%s" % af,
-            "dsn": "dbname=pubsub",
+    conf = {"dsn": "dbname=pubsub",
             "channel": "test",
-            "event_worker": TestWorker}
+            }
 
     specs = [
-        (EventArbiter, 30, "supervisor", {}, "tcp_pool"),
-        (PgEventListener, 30, "worker", {}, "listener"),
+        (TestWorker, 30, "worker", {}, "listener"),
     ]
 
     arbiter = Arbiter(conf, specs)
